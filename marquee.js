e67bdbe0419363fdf06386c9945f1f3336bb6fda
Danin Na <apps.danin@gmail.com>
2025-02-23 16:14:51 -0800

    marquee first init after rechange to v02

--- marquee.js
+++ marquee.js
@@ -1,76 +1,97 @@
-class Marquee01
+class Icon_01
 {
-    #Comp
+    #rf = {
+        worker: {
+            tag: { target: null },
+            opt: { source: null },
+        },
+    }
 
     constructor(config = {})
     {
+        this.#rf.worker.tag.target = config.rf.worker.id.target // data-rf-worker-id-targetID
+        this.#rf.worker.opt.source = config.rf.worker.opt.source // data-rf-worker-tag-source
+        console.log(config)
+    }
+    work ()
+    {
+        document.querySelectorAll(`[${this.#rf.worker.tag.target}]`).forEach((e) =>
+        {
+            e.innerHTML = decodeURIComponent(e.getAttribute(this.#rf.worker.opt.source))
+        })
+        return this
+    }
+}
 
-        //Set Attr
-        this.#Comp = {}
-        this.#Comp.el = {}
-        this.#Comp.sync = {}
-        this.#Comp.opt = {}
-        this.#Comp.prog = {}
 
-        /**
-         * data-rf-comp-
-         */
+class Marquee_01
+{
+    #rf = {
+        component: {
+            e: {
+                tag: { self, slider },
+                opt: { ease, duration, direction },
+                prog: { delay, time, anim }
+            }
+        }
+    }
 
-        this.#Comp.el.container = config.el.container
-        this.#Comp.el.slide = config.el.slide
 
-        this.#Comp.sync.container = null
-        this.#Comp.sync.slide = null
+    constructor(config = {})
+    {
+        this.#rf.component.e.tag.self = config.rf.component.e.tag.self                          // rf-component-e-tag-self
+        this.#rf.component.e.tag.slider = config.rf.component.e.tag.slider                      // rf-component-e-tag-slider
 
-        this.#Comp.opt.ease = config.opt.ease || 'none'
-        this.#Comp.opt.direction = config.opt.direction || 'left'
-        this.#Comp.opt.duration = config.opt.duration || 30
+        this.#rf.component.e.opt.ease = config.rf.component.e.opt.ease || 'none';               // rf-component-e-opt-ease
+        this.#rf.component.e.opt.duration = config.rf.component.e.opt.duration || 30;           // rf-component-e-opt-duration
+        this.#rf.component.e.opt.direction = config.rf.component.e.opt.direction || 'left';     // rf-component-e-opt-diraction
 
-        this.#Comp.prog.val = 0
-        this.#Comp.prog.time = null
-        this.#Comp.prog.anim = null
-        this.#Comp.prog.delay = 200
+        this.#rf.component.e.prog.delay = 200
+        this.#rf.component.e.prog.time = null;
+        this.#rf.component.e.prog.anim = null;
 
-        this.#Comp.el.slide.setAttribute('data-slide-item', '')
-        this.#Comp.el.container.append(this.#Comp.el.slide.cloneNode(true))
+        // Set data attribute using new naming convention
+        this.#rf.component.e.tag.slider.setAttribute('rf-component-self-selector', '');
+        this.#rf.component.e.tag.self.append(this.#rf.component.e.tag.slider.cloneNode(true));
     }
 
     #reset (animation)
     {
-        if (!animation) return 0
-        const savedProgress = animation.progress()
-        animation.progress(0).kill()
-        return savedProgress
+        if (!animation) return 0;
+        const savedProgress = animation.progress();
+        animation.progress(0).kill();
+        return savedProgress;
     }
 
     #render ()
     {
-        const prog = this.#reset(this.#Comp.prog.anim)
-        const items = this.#Comp.el.container.querySelectorAll('[data-slide-item]')
-        const width = parseInt(getComputedStyle(items[0]).width, 10)
-        const [xFrom, xTo] = this.#Comp.opt.direction === 'left' ? [0, -width] : [-width, 0]
+        const prog = this.#reset(this.#rf.component.e.prog.anim);
+        const items = this.#rf.component.e.tag.self.querySelectorAll('[rf-component-self-selector]');
+        const width = parseInt(getComputedStyle(items[0]).width, 10);
+        const [xFrom, xTo] = this.#rf.component.e.opt.direction === 'left'
+            ? [0, -width]
+            : [-width, 0];
 
-        this.#Comp.prog.anim = gsap.fromTo(
+        this.#rf.component.e.prog.anim = gsap.fromTo(
             items,
             { x: xFrom },
             {
                 x: xTo,
-                duration: this.#Comp.opt.duration,
-                ease: this.#Comp.opt.ease,
+                duration: this.#rf.component.e.opt.duration,
+                ease: this.#rf.component.e.opt.ease,
                 repeat: -1,
             }
-        )
-        this.#Comp.prog.anim.progress(prog)
+        );
+        this.#rf.component.e.prog.anim.progress(prog);
     }
 
     run ()
     {
-
-        this.#render()
+        this.#render();
         window.addEventListener('resize', () =>
         {
-            clearTimeout(this.#Comp.prog.time)
-            this.#Comp.prog.time = setTimeout(() => this.#render(), this.#Comp.prog.delay)
-        })
+            clearTimeout(this.#rf.component.e.prog.time);
+            this.#rf.component.e.prog.time = setTimeout(() => this.#render(), this.#rf.component.e.prog.delay);
+        });
     }
 }
